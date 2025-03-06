9ef7a7be2b00e4038dbb293ae32912c7f543acd2
Lilnait-s <style.damn.falling@gmail.com>
2025-03-05 21:57:52 -0500

    feat: enhance Home component by integrating Hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { Hero } from "@/components/home/hero"
+
 export default function Home() {
-  return <div>home</div>
+  return (
+    <div className="space-y-20 mb-20 px-3">
+      <Hero />
+    </div>
+  )
 }
