5ff7a0b74130febb86a6d9427522fab009c12f76
Lilnait-s <style.damn.falling@gmail.com>
2025-03-10 21:03:06 -0500

    feat: integrate FeaturedProducts and SizesSection components into Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { FeaturedProducts } from "@/components/home/featured-products"
 import { Hero } from "@/components/home/hero"
+import { SizesSection } from "@/components/home/sizes"
 
 export default function Home() {
   return (
     <div className="space-y-20 mb-20 px-3">
       <Hero />
+      <FeaturedProducts />
+      <SizesSection />
     </div>
   )
 }
