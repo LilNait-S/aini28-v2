e73da79d7784f2e9cb67a8dacefdcb023f007427
Lilnait-s <style.damn.falling@gmail.com>
2025-03-10 21:02:32 -0500

    feat: add PRODUCT constant to define product data structure

---
+++ constants/products.ts
@@ -0,0 +1,388 @@
+import { Product } from "@/types/product"
+
+export const PRODUCT: Product[] = [
+  {
+    id: 1,
+    urlImage:
+      "https://cdn.sanity.io/images/cod9urb8/production/7dcc288b9771d914315b6e5762f7b588a9061fc3-800x800.jpg",
+    title: "Doraemon disfraz lotso y stich peluche de juguete",
+    sizes: [
+      {
+        id: 1,
+        label: "Pequeño",
+        price: 10.99,
+        size: 20,
+      },
+      {
+        id: 2,
+        label: "Mediano",
+        price: 12.99,
+        size: 30,
+      },
+      {
+        id: 3,
+        label: "Grande",
+        price: 14.99,
+        size: 40,
+      },
+    ],
+  },
+  {
+    id: 2,
+    urlImage:
+      "https://cdn.sanity.io/images/cod9urb8/production/7dcc288b9771d914315b6e5762f7b588a9061fc3-800x800.jpg",
+    title: "Producto 2",
+    sizes: [
+      {
+        id: 4,
+        label: "Pequeño",
+        price: 11.99,
+        size: 20,
+      },
+      {
+        id: 5,
+        label: "Mediano",
+        price: 13.99,
+        size: 30,
+      },
+    ],
+  },
+  {
+    id: 3,
+    urlImage:
+      "https://cdn.sanity.io/images/cod9urb8/production/7dcc288b9771d914315b6e5762f7b588a9061fc3-800x800.jpg",
+    title: "Producto 3",
+    sizes: [
+      {
+        id: 6,
+        label: "Pequeño",
+        price: 9.99,
+        size: 20,
+      },
+      {
+        id: 7,
+        label: "Mediano",
+        price: 11.99,
+        size: 30,
+      },
+      {
+        id: 8,
+        label: "Grande",
+        price: 13.99,
+        size: 40,
+      },
+      {
+        id: 9,
+        label: "Gigante",
+        price: 15.99,
+        size: 50,
+      },
+    ],
+  },
+  {
+    id: 4,
+    urlImage:
+      "https://cdn.sanity.io/images/cod9urb8/production/7dcc288b9771d914315b6e5762f7b588a9061fc3-800x800.jpg",
+    title: "Producto 4",
+    sizes: [
+      {
+        id: 10,
+        label: "Pequeño",
+        price: 8.99,
+        size: 20,
+      },
+      {
+        id: 11,
+        label: "Mediano",
+        price: 10.99,
+        size: 30,
+      },
+    ],
+  },
+  {
+    id: 5,
+    urlImage:
+      "https://cdn.sanity.io/images/cod9urb8/production/7dcc288b9771d914315b6e5762f7b588a9061fc3-800x800.jpg",
+    title: "Producto 5",
+    sizes: [
+      {
+        id: 12,
+        label: "Pequeño",
+        price: 7.99,
+        size: 20,
+      },
+      {
+        id: 13,
+        label: "Mediano",
+        price: 9.99,
+        size: 30,
+      },
+      {
+        id: 14,
+        label: "Grande",
+        price: 11.99,
+        size: 40,
+      },
+    ],
+  },
+  {
+    id: 6,
+    urlImage:
+      "https://cdn.sanity.io/images/cod9urb8/production/7dcc288b9771d914315b6e5762f7b588a9061fc3-800x800.jpg",
+    title: "Producto 6",
+    sizes: [
+      {
+        id: 15,
+        label: "Pequeño",
+        price: 6.99,
+        size: 20,
+      },
+      {
+        id: 16,
+        label: "Mediano",
+        price: 8.99,
+        size: 30,
+      },
+      {
+        id: 17,
+        label: "Grande",
+        price: 10.99,
+        size: 40,
+      },
+    ],
+  },
+  {
+    id: 7,
+    urlImage:
+      "https://cdn.sanity.io/images/cod9urb8/production/7dcc288b9771d914315b6e5762f7b588a9061fc3-800x800.jpg",
+    title: "Producto 7",
+    sizes: [
+      {
+        id: 18,
+        label: "Pequeño",
+        price: 5.99,
+        size: 20,
+      },
+      {
+        id: 19,
+        label: "Mediano",
+        price: 7.99,
+        size: 30,
+      },
+      {
+        id: 20,
+        label: "Grande",
+        price: 9.99,
+        size: 40,
+      },
+    ],
+  },
+  {
+    id: 8,
+    urlImage:
+      "https://cdn.sanity.io/images/cod9urb8/production/7dcc288b9771d914315b6e5762f7b588a9061fc3-800x800.jpg",
+    title: "Producto 8",
+    sizes: [
+      {
+        id: 21,
+        label: "Pequeño",
+        price: 4.99,
+        size: 20,
+      },
+      {
+        id: 22,
+        label: "Mediano",
+        price: 6.99,
+        size: 30,
+      },
+      {
+        id: 23,
+        label: "Grande",
+        price: 8.99,
+        size: 40,
+      },
+    ],
+  },
+  {
+    id: 9,
+    urlImage:
+      "https://cdn.sanity.io/images/cod9urb8/production/7dcc288b9771d914315b6e5762f7b588a9061fc3-800x800.jpg",
+    title: "Producto 9",
+    sizes: [
+      {
+        id: 24,
+        label: "Pequeño",
+        price: 3.99,
+        size: 20,
+      },
+      {
+        id: 25,
+        label: "Mediano",
+        price: 5.99,
+        size: 30,
+      },
+      {
+        id: 26,
+        label: "Grande",
+        price: 7.99,
+        size: 40,
+      },
+    ],
+  },
+  {
+    id: 10,
+    urlImage:
+      "https://cdn.sanity.io/images/cod9urb8/production/7dcc288b9771d914315b6e5762f7b588a9061fc3-800x800.jpg",
+    title: "Producto 10",
+    sizes: [
+      {
+        id: 27,
+        label: "Pequeño",
+        price: 2.99,
+        size: 20,
+      },
+      {
+        id: 28,
+        label: "Mediano",
+        price: 4.99,
+        size: 30,
+      },
+      {
+        id: 29,
+        label: "Grande",
+        price: 6.99,
+        size: 40,
+      },
+    ],
+  },
+  {
+    id: 11,
+    urlImage:
+      "https://cdn.sanity.io/images/cod9urb8/production/7dcc288b9771d914315b6e5762f7b588a9061fc3-800x800.jpg",
+    title: "Producto 11",
+    sizes: [
+      {
+        id: 30,
+        label: "Pequeño",
+        price: 1.99,
+        size: 20,
+      },
+      {
+        id: 31,
+        label: "Mediano",
+        price: 3.99,
+        size: 30,
+      },
+      {
+        id: 32,
+        label: "Grande",
+        price: 5.99,
+        size: 40,
+      },
+    ],
+  },
+  {
+    id: 12,
+    urlImage:
+      "https://cdn.sanity.io/images/cod9urb8/production/7dcc288b9771d914315b6e5762f7b588a9061fc3-800x800.jpg",
+    title: "Producto 12",
+    sizes: [
+      {
+        id: 33,
+        label: "Pequeño",
+        price: 0.99,
+        size: 20,
+      },
+      {
+        id: 34,
+        label: "Mediano",
+        price: 2.99,
+        size: 30,
+      },
+      {
+        id: 35,
+        label: "Grande",
+        price: 4.99,
+        size: 40,
+      },
+    ],
+  },
+  {
+    id: 13,
+    urlImage:
+      "https://cdn.sanity.io/images/cod9urb8/production/7dcc288b9771d914315b6e5762f7b588a9061fc3-800x800.jpg",
+    title: "Producto 13",
+    sizes: [
+      {
+        id: 36,
+        label: "Pequeño",
+        price: 12.99,
+        size: 20,
+      },
+      {
+        id: 37,
+        label: "Mediano",
+        price: 14.99,
+        size: 30,
+      },
+      {
+        id: 38,
+        label: "Grande",
+        price: 16.99,
+        size: 40,
+      },
+    ],
+  },
+  {
+    id: 14,
+    urlImage:
+      "https://cdn.sanity.io/images/cod9urb8/production/7dcc288b9771d914315b6e5762f7b588a9061fc3-800x800.jpg",
+    title: "Producto 14",
+    sizes: [
+      {
+        id: 39,
+        label: "Pequeño",
+        price: 13.99,
+        size: 20,
+      },
+      {
+        id: 40,
+        label: "Mediano",
+        price: 15.99,
+        size: 30,
+      },
+      {
+        id: 41,
+        label: "Grande",
+        price: 17.99,
+        size: 40,
+      },
+    ],
+  },
+  {
+    id: 15,
+    urlImage:
+      "https://cdn.sanity.io/images/cod9urb8/production/7dcc288b9771d914315b6e5762f7b588a9061fc3-800x800.jpg",
+    title: "Producto 15",
+    sizes: [
+      {
+        id: 42,
+        label: "Pequeño",
+        price: 14.99,
+        size: 20,
+      },
+      {
+        id: 43,
+        label: "Mediano",
+        price: 16.99,
+        size: 30,
+      },
+      {
+        id: 44,
+        label: "Grande",
+        price: 18.99,
+        size: 40,
+      },
+    ],
+  },
+]
