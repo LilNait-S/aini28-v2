bfbe907832f634bdd1ab3726e60a9cea73f29fd5
Lilnait-s <style.damn.falling@gmail.com>
2025-03-12 19:59:12 -0500

    feat: replace div with header in FeaturedProducts for semantic structure

--- components/home/featured-products.tsx
+++ components/home/featured-products.tsx
@@ -7,14 +7,14 @@ import { ScrollArea, ScrollBar } from "../ui/scroll-area"
 export function FeaturedProducts() {
   return (
     <section className="flex flex-col space-y-2">
-      <div className="flex justify-between items-center">
+      <header className="flex justify-between items-center">
         <h2 className="text-2xl font-bold text-primary">
           Productos destacados
         </h2>
         <Link href="#" className={buttonVariants({ variant: "link" })}>
           Ver más
         </Link>
-      </div>
+      </header>
       <ScrollArea className="whitespace-nowrap">
         <div className="flex w-max space-x-8 pb-4 pl-4">
           {PRODUCT.map((product) => (
