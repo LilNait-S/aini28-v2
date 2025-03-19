7dd5b2e847942f2bed0f9c29dea17d76af83c8b8
Lilnait-s <style.damn.falling@gmail.com>
2025-03-18 21:52:54 -0500

    feat: add IconStar component for enhanced iconography

--- components/icons/index.tsx
+++ components/icons/index.tsx
@@ -23,6 +23,7 @@ export const IconTikTok = ({ className }: { className?: string }) => (
     <path d="M19.367 7.29A5 5 0 0 1 18.13 4h-3.605v14.467a3.024 3.024 0 0 1-3.022 2.916A3.04 3.04 0 0 1 8.47 18.35c0-2.007 1.937-3.512 3.932-2.893V11.77c-4.025-.537-7.549 2.59-7.549 6.58 0 3.885 3.22 6.65 6.639 6.65 3.663 0 6.638-2.975 6.638-6.65v-7.338a8.57 8.57 0 0 0 5.017 1.61V9.017s-2.194.105-3.78-1.727"></path>
   </svg>
 )
+
 export const IconFacebook = ({ className }: { className?: string }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -33,5 +34,20 @@ export const IconFacebook = ({ className }: { className?: string }) => (
     className={className}
   >
     <path d="M25.667 14.5C25.667 8.06 20.44 2.833 14 2.833S2.333 8.06 2.333 14.5c0 5.647 4.014 10.348 9.334 11.433V18H9.333v-3.5h2.334v-2.917A4.09 4.09 0 0 1 15.75 7.5h2.917V11h-2.334a1.17 1.17 0 0 0-1.166 1.167V14.5h3.5V18h-3.5v8.108c5.891-.583 10.5-5.553 10.5-11.608"></path>
+  </svg>
+)
+
+export const IconStar = ({ className }: { className?: string }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="60"
+    height="60"
+    fill="none"
+    viewBox="0 0 60 60"
+    className={className}
+  >
+    <path
+      d="M58.125 23.625H36.656L30 2.813l-6.656 20.812H1.875l17.344 12.844-6.563 20.719L30 44.344l17.344 12.843-6.657-20.812z"
+    ></path>
   </svg>
 )
