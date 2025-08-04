620d3843cb5025a5cd5d11677b3a5f1292041e5a
Nait-s <lilnait.santos@gmail.com>
2025-08-04 09:33:19 -0500

    fix: update phone number and WhatsApp URL in COMPANY constant

--- constants/phone-company.ts
+++ constants/phone-company.ts
@@ -2,6 +2,6 @@ export const COMPANY = {
   name: "Aini28",
   web: "https://aini28.com",
   email: "aini28.onlinestore@gmail.com",
-  phoneNumber: "51932525805",
-  whatsappUrl: "https://wa.me/51932525805",
+  phoneNumber: "51927742710",
+  whatsappUrl: "https://wa.me/51927742710",
 }
